84962e39b06d600ede6efc4ccb1462de560556e1
José Lima <joselima.engineering@gmail.com>
2018-08-16 21:44:48 +0200

    Add WPF project. Status: display metadata attributes in an ItemsControl.

---
+++ joselima.dicom.viewer.wpf/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using joselima.dicom.viewer.wpf.ViewModels;
+using joselima.dicom.viewer.wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace joselima.dicom.viewer.wpf {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+
+        protected override void OnStartup(StartupEventArgs e) {
+
+            base.OnStartup(e);
+
+            var testFile1 = @"C:\Users\user1\Documents\DICOM\testData\Anonymized20180815\series-000000\image-000000.dcm";
+
+            var dicomFile = new FileParser().Parse(testFile1, 0x7FE0000F);
+
+            var dataSetVM = new DataSetViewModel() {
+                Items = dicomFile.Attributes.Select(x => 
+                    new AttributeViewModel() {
+                        Tag = x.Value.Tag.ToString(),
+                        Value = x.Value.Value.ToString().Trim(),
+                    }).ToList()
+            };
+
+            var mainWindow = new MainWindow() {
+                DataContext = dataSetVM
+            };
+            mainWindow.Show();
+        }
+    }
+}
---
+++ joselima.dicom.viewer.wpf/ViewModels/DataSetViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace joselima.dicom.viewer.wpf.ViewModels {
+
+    public class DataSetViewModel {
+
+        public List<AttributeViewModel> Items { get; set; }
+    }
+
+    public class AttributeViewModel {
+
+        public string Tag { get; set; }
+        public string Value { get; set; }
+    }
+}
